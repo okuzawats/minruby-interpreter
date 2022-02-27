3ffab7e42da207b55a7c128aacfacb450ce79c6b
okuzawats <okuzawats@gmail.com>
2022-02-27 21:40:32 +0900

    fix test case names

--- test/test_sample.rb
+++ test/test_sample.rb
@@ -2,11 +2,11 @@ require 'minitest/autorun'
 require_relative '../lib/sample'
 
 class TestSample < Minitest::Test
-  def test_answer_return_true_if_42
+  def testAnswerReturnTrueIf42
     assert_equal true, answer(42)
   end
 
-  def test_answer_return_false_if_not_42
+  def testAnswerReturnFalseIfNot42
     assert_equal false, answer(0)
   end
 end
