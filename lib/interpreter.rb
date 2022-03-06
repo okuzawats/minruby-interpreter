5441533ea80dac88e5a5da9adb29b2a87bb280b7
okuzawats <okuzawats@gmail.com>
2022-03-06 16:09:24 +0900

    implement statement

--- lib/interpreter.rb
+++ lib/interpreter.rb
@@ -53,5 +53,13 @@ def evaluate(tree)
   when 'func_call'
     # TODO
     p(evaluate(tree[2]))
+  when 'stmts'
+    i = 1
+    last = nil
+    while tree[i] != nil
+      last = evaluate(tree[i])
+      i = i + 1
+    end
+    last
   end
 end
