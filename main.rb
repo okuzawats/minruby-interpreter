07b8fabf29d7c271fe1c71a495da6d9f8cef2c26
okuzawats <okuzawats@gmail.com>
2022-03-06 16:34:01 +0900

    lint fix

--- main.rb
+++ main.rb
@@ -2,6 +2,6 @@ require 'minruby'
 require_relative './lib/interpreter'
 require_relative './lib/sample'
 
-str = minruby_load()
+str = minruby_load
 tree = minruby_parse(str)
 answer = evaluate(tree)
