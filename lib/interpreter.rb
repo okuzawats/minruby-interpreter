58c78d9d113f9a4388c1518310a3b4a430d910aa
okuzawats <okuzawats@gmail.com>
2022-03-08 08:16:38 +0900

    rename env to lenv

--- lib/interpreter.rb
+++ lib/interpreter.rb
@@ -4,82 +4,82 @@ def execute(code)
   evaluate(minruby_parse(code))
 end
 
-def evaluate(tree, env = {})
+def evaluate(tree, lenv = {})
   case tree[0]
   when 'if'
-    if evaluate(tree[1], env)
-      evaluate(tree[2], env)
+    if evaluate(tree[1], lenv)
+      evaluate(tree[2], lenv)
     else
-      evaluate(tree[3], env)
+      evaluate(tree[3], lenv)
     end
   when 'while'
-    while evaluate(tree[1], env)
-      evaluate(tree[2], env)
+    while evaluate(tree[1], lenv)
+      evaluate(tree[2], lenv)
     end
   when 'lit'
     tree[1]
   when '+'
-    left = evaluate(tree[1], env)
-    right = evaluate(tree[2], env)
+    left = evaluate(tree[1], lenv)
+    right = evaluate(tree[2], lenv)
     left + right
   when '-'
-    left = evaluate(tree[1], env)
-    right = evaluate(tree[2], env)
+    left = evaluate(tree[1], lenv)
+    right = evaluate(tree[2], lenv)
     left - right
   when '*'
-    left = evaluate(tree[1], env)
-    right = evaluate(tree[2], env)
+    left = evaluate(tree[1], lenv)
+    right = evaluate(tree[2], lenv)
     left * right
   when '/'
-    left = evaluate(tree[1], env)
-    right = evaluate(tree[2], env)
+    left = evaluate(tree[1], lenv)
+    right = evaluate(tree[2], lenv)
     left / right
   when '%'
-    left = evaluate(tree[1], env)
-    right = evaluate(tree[2], env)
+    left = evaluate(tree[1], lenv)
+    right = evaluate(tree[2], lenv)
     left % right
   when '**'
-    left = evaluate(tree[1], env)
-    right = evaluate(tree[2], env)
+    left = evaluate(tree[1], lenv)
+    right = evaluate(tree[2], lenv)
     left**right
   when '=='
-    left = evaluate(tree[1], env)
-    right = evaluate(tree[2], env)
+    left = evaluate(tree[1], lenv)
+    right = evaluate(tree[2], lenv)
     left == right
   when '!='
-    left = evaluate(tree[1], env)
-    right = evaluate(tree[2], env)
+    left = evaluate(tree[1], lenv)
+    right = evaluate(tree[2], lenv)
     left != right
   when '>'
-    left = evaluate(tree[1], env)
-    right = evaluate(tree[2], env)
+    left = evaluate(tree[1], lenv)
+    right = evaluate(tree[2], lenv)
     left > right
   when '>='
-    left = evaluate(tree[1], env)
-    right = evaluate(tree[2], env)
+    left = evaluate(tree[1], lenv)
+    right = evaluate(tree[2], lenv)
     left >= right
   when '<'
-    left = evaluate(tree[1], env)
-    right = evaluate(tree[2], env)
+    left = evaluate(tree[1], lenv)
+    right = evaluate(tree[2], lenv)
     left < right
   when '<='
-    left = evaluate(tree[1], env)
-    right = evaluate(tree[2], env)
+    left = evaluate(tree[1], lenv)
+    right = evaluate(tree[2], lenv)
     left <= right
   when 'func_call'
     # TODO
-    p(evaluate(tree[2], env))
+    p(evaluate(tree[2], lenv))
   when 'stmts'
     i = 1
     last = nil
     while !tree[i].nil?
-      last = evaluate(tree[i], env)
+      last = evaluate(tree[i], lenv)
       i += 1
     end
     last
   when 'var_assign'
-    env[tree[1]] = evaluate(tree[2], env)
+    lenv[tree[1]] = evaluate(tree[2], lenv)
   when 'var_ref'
-    env[tree[1]]
+    lenv[tree[1]]
   end
 end
