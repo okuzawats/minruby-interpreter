c17db46bee14535baee85ac55b1966e686dbfb6c
okuzawats <okuzawats@gmail.com>
2022-03-07 09:00:07 +0900

    implement while

--- lib/interpreter.rb
+++ lib/interpreter.rb
@@ -6,6 +6,10 @@ def evaluate(tree, env = {})
     else
       evaluate(tree[3], env)
     end
+  when 'while'
+    while evaluate(tree[1], env)
+      evaluate(tree[2], env)
+    end
   when 'lit'
     tree[1]
   when '+'
